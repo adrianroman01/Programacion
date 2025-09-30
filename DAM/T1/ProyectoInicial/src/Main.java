13413d8bf6e415eea56c899eaff044f3005de689
adrianroman01 <adriansanz580@gmail.com>
2025-09-30 21:04:37 +0200

    aa

--- DAM/T1/ProyectoInicial/src/Main.java
+++ DAM/T1/ProyectoInicial/src/Main.java
@@ -8,21 +8,11 @@ public class Main {
     public static void main (String []Args) {
 
 
-        for (int i = 0; i <= 100; i = i + 1) {
 
 
-
-            
-
-            }
-
-
-        }
+        System.out.println("");
 
 
     }
-
-
-
 
 }
